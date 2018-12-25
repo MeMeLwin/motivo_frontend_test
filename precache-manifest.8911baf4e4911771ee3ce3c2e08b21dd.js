e4df6c9d005a43ce90cbc5b5f76baf530633cbc2
MeMeLwin <lwin.meme993@gmail.com>
2018-12-25 15:28:21 +0630

    Updates

---
+++ precache-manifest.8911baf4e4911771ee3ce3c2e08b21dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d44fcff7446efe72deb",
+    "url": "/motivo_frontend_test/static/js/runtime~main.2d44fcff.js"
+  },
+  {
+    "revision": "f1cc9cce12233f7d162d",
+    "url": "/motivo_frontend_test/static/js/main.f1cc9cce.chunk.js"
+  },
+  {
+    "revision": "765aac18f2fdd8235e5a",
+    "url": "/motivo_frontend_test/static/js/1.765aac18.chunk.js"
+  },
+  {
+    "revision": "f1cc9cce12233f7d162d",
+    "url": "/motivo_frontend_test/static/css/main.bd550fda.chunk.css"
+  },
+  {
+    "revision": "765aac18f2fdd8235e5a",
+    "url": "/motivo_frontend_test/static/css/1.ff934958.chunk.css"
+  },
+  {
+    "revision": "740f4d6f14ca041080e9576cbbdb7c96",
+    "url": "/motivo_frontend_test/index.html"
+  }
+];
