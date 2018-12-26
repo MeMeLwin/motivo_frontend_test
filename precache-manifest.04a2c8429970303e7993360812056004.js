171c7db375bba5b1646cd3d3490001122ce4398a
MeMeLwin <lwin.meme993@gmail.com>
2018-12-26 18:54:46 +0630

    Updates

---
+++ precache-manifest.04a2c8429970303e7993360812056004.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d44fcff7446efe72deb",
+    "url": "/motivo_frontend_test/static/js/runtime~main.2d44fcff.js"
+  },
+  {
+    "revision": "a53f15f0284954a6f982",
+    "url": "/motivo_frontend_test/static/js/main.a53f15f0.chunk.js"
+  },
+  {
+    "revision": "c08078168f87f868414d",
+    "url": "/motivo_frontend_test/static/js/1.c0807816.chunk.js"
+  },
+  {
+    "revision": "a53f15f0284954a6f982",
+    "url": "/motivo_frontend_test/static/css/main.f13ce567.chunk.css"
+  },
+  {
+    "revision": "c08078168f87f868414d",
+    "url": "/motivo_frontend_test/static/css/1.0e252b6e.chunk.css"
+  },
+  {
+    "revision": "c840e60c2045850a620c87ace3d361d9",
+    "url": "/motivo_frontend_test/index.html"
+  }
+];
