74ae9b49888ba20a1b19360d0753ad308a11b2a7
MeMeLwin <lwin.meme993@gmail.com>
2018-12-28 16:04:18 +0630

    Updates

--- precache-manifest.40ac24cf8261938d17519c6ff66ab049.js
+++ precache-manifest.40ac24cf8261938d17519c6ff66ab049.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/motivo_frontend_test/static/js/runtime~main.2d44fcff.js"
   },
   {
-    "revision": "c55df122d059f2e01c3a",
-    "url": "/motivo_frontend_test/static/js/main.c55df122.chunk.js"
+    "revision": "d4f498c0566a74c002a6",
+    "url": "/motivo_frontend_test/static/js/main.d4f498c0.chunk.js"
   },
   {
     "revision": "2120cf382917366d6501",
     "url": "/motivo_frontend_test/static/js/1.2120cf38.chunk.js"
   },
   {
-    "revision": "c55df122d059f2e01c3a",
-    "url": "/motivo_frontend_test/static/css/main.074c20eb.chunk.css"
+    "revision": "d4f498c0566a74c002a6",
+    "url": "/motivo_frontend_test/static/css/main.62113fa4.chunk.css"
   },
   {
-    "revision": "6a0d467d87ffc3f6434e90931d57d985",
+    "revision": "188a82f0cebe398aea7a492b2441a1ba",
     "url": "/motivo_frontend_test/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/motivo_frontend_test/precache-manifest.e351743f1b51871ad436c8f13288c799.js"
+  "/motivo_frontend_test/precache-manifest.40ac24cf8261938d17519c6ff66ab049.js"
 );
 
 workbox.clientsClaim();
